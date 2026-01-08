98a448b9183e85b035b9055493779eecbc992e9c
GOA-neurons <yewintthetlwin@gmail.com>
2026-01-08 15:07:37 +0630

    Refactor sync function to start and streamline code

--- optimized_evolution.js
+++ optimized_evolution.js
@@ -1,19 +1,17 @@
 const admin = require('firebase-admin');
 const { Client } = require('pg');
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+admin.initializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)) });
 const db = admin.firestore();
-
-async function sync() {
+async function start() {
   const client = new Client({ connectionString: process.env.NEON_DATABASE_URL, ssl: { rejectUnauthorized: false } });
-  try {
-    await client.connect();
-    const snap = await db.collection('neurons').limit(5).get();
-    for (const doc of snap.docs) {
-      await client.query('INSERT INTO neurons (data) VALUES ($1)', [JSON.stringify(doc.data())]);
-    }
-    console.log('✅ GOA Sync Success!');
-  } catch (e) { console.error(e); } finally { await client.end(); }
+  await client.connect();
+  const snap = await db.collection('neurons').limit(5).get();
+  for (const doc of snap.docs) {
+    await client.query('INSERT INTO neurons (data) VALUES ($1)', [JSON.stringify(doc.data())]);
+  }
+  await client.end();
+  console.log('DONE');
 }
-sync();
+start();
+
+
